a1f4abadcc582d50be24adea524ee65b6aa63686
Leonid Krapchatov <hellsing1998nsk@gmail.com>
2025-01-31 13:13:46 +0300

    Add tls and https configuration

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"context"
+	"crypto/tls"
 	"flag"
 	"fmt"
 	"log/slog"
@@ -32,6 +33,17 @@ func Run(ctx context.Context) error {
 
 	msgs := storage.New()
 
+	var tlsConfig *tls.Config
+	if isProdEnv {
+		cert, err := tls.LoadX509KeyPair(config.Certificate.Cert, config.Certificate.Key)
+		if err != nil {
+			return err
+		}
+		tlsConfig = &tls.Config{
+			Certificates: []tls.Certificate{cert},
+		}
+	}
+
 	router := chi.NewRouter()
 	assetsFS, err := web.AssetsFS()
 	if err != nil {
@@ -51,6 +63,9 @@ func Run(ctx context.Context) error {
 		WriteTimeout: config.Timeout,
 		IdleTimeout:  config.IdleTimeout,
 	}
+	if isProdEnv {
+		server.TLSConfig = tlsConfig
+	}
 
 	go func() {
 		<-ctx.Done()
@@ -60,8 +75,14 @@ func Run(ctx context.Context) error {
 
 	startingMsg := fmt.Sprintf("--- Starting server on %s", config.Address)
 	slog.Info(startingMsg)
-	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		return err
+	if isProdEnv {
+		if err := server.ListenAndServeTLS("", ""); err != nil && err != http.ErrServerClosed {
+			return err
+		}
+	} else {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			return err
+		}
 	}
 
 	return nil
--- internal/config/config.go
+++ internal/config/config.go
@@ -9,15 +9,21 @@ import (
 )
 
 type Config struct {
-	Env        string `yaml:"env" env-required:"true"`
-	AssetsDir  string `yaml:"assets_dir"`
-	HTTPServer `yaml:"http_server"`
+	Env         string `yaml:"env" env-required:"true"`
+	AssetsDir   string `yaml:"assets_dir"`
+	HTTPServer  `yaml:"http_server"`
+	Certificate `yaml:"certificate"`
 }
 
 type HTTPServer struct {
 	Address     string        `yaml:"address" env-required:"true"`
 	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
 	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+}
+
+type Certificate struct {
+	Cert string `yaml:"cert"`
+	Key  string `yaml:"key"`
 }
 
 // Load configuration file or call Fatalf
